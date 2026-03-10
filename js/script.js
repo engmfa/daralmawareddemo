341977ae5fb01eebbb3a5ce928f0850bd0e70537
Abu Abdullah <47968540+ENGMFA@users.noreply.github.com>
2026-03-11 00:47:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const nav = document.querySelector(".nav-list");
 toggleBtn.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+// الكود المضاف (لإغلاق القائمة عند النقر على رابط)
+document.querySelectorAll(".nav-list a").forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
